522d0bc303f37573f05ca664718f8a925f7f7752
j-maestre <xemaiestacio@gmail.com>
2022-06-22 00:58:24 +0200

    add 2 first options,trying new solutions

--- gameController.js
+++ gameController.js
@@ -1,5 +1,8 @@
-let turno = 0; //0-> j1, 1-> maquina
+const MAX_TRYES = 100;
+
+let turno = 1; //0-> j1, 1-> maquina
 let playerCount = 0; //Total de casillas marcadas 0-3
+let IACount = 0;
 let gameOver = false;
 
 
@@ -14,6 +17,7 @@ let casillaMarcada6 = 0;
 let casillaMarcada7 = 0;
 let casillaMarcada8 = 0;
 
+
 let casilla0 = document.getElementById("casilla0");
 let casilla1 = document.getElementById("casilla1");
 let casilla2 = document.getElementById("casilla2");
@@ -23,6 +27,323 @@ let casilla5= document.getElementById("casilla5");
 let casilla6 = document.getElementById("casilla6");
 let casilla7 = document.getElementById("casilla7");
 let casilla8 = document.getElementById("casilla8");
+
+function IATime(input){
+    console.log("-----------Calculando-----------");
+    // let remove = Math.ceil((Math.round(resultado.delete*100)/10));
+    // let newAdd = Math.ceil((Math.round(resultado.add*100)/10));
+
+    let error = false;
+    let totalTryes = 0;
+
+
+        totalTryes++;
+        // console.log("Intentos-> "+totalTryes);
+        let resultado = network.run(input);
+        console.log(resultado)
+        let arr = [];
+        arr = resultado;
+        //TODO ver que resultado tiene el valor mas alto, ese será el proximo movimiento
+        // let remove = Math.round(((resultado.delete + Number.EPSILON) * 100)/10);
+        // let newAdd = Math.round(((resultado.add + Number.EPSILON) * 100)/10);
+        let remove = 0;
+        let newAdd = -1;
+        let max = -1;
+        for(let i = 0; i < 8; i++){
+            if(resultado[i]>max){
+                newAdd=i;
+                max = resultado[i];
+            }
+        }
+        // console.log("Remove-> "+remove+" NewAdd-> "+newAdd);
+        // newAdd = Math.round(((newAdd + Number.EPSILON) * 100)/10);
+        console.log("Remove-> "+remove+" NewAdd-> "+newAdd);
+
+        
+        if(IACount<3){
+            
+            //Comprobamos que la casilla que ha marcado la IA no está marcada
+            switch(newAdd){
+                case(0):
+                    if(casillaMarcada0 == 0){
+                        console.log("se puede 0");
+                        casillaMarcada0=2;
+                        casilla0.classList.add("IAMark");
+                    }else{
+                        console.log("No se puede");
+                        error = true;
+                    }
+                break;
+                    case(1):
+                    if(casillaMarcada1 == 0){
+                        console.log("se puede 1");
+                        casillaMarcada1=2;
+                        casilla1.classList.add("IAMark");
+                    }else{
+                        console.log("No se puede");
+                        error = true;
+                    }
+                break;
+                    case(2):
+                    if(casillaMarcada2 == 0){
+                        console.log("se puede 2");
+                        casillaMarcada2=2;
+                        casilla2.classList.add("IAMark");
+                    }else{
+                        console.log("No se puede");
+                        error = true;
+                    }
+                break;
+                    case(3):
+                    if(casillaMarcada3 == 0){
+                        console.log("se puede 3");
+                        casillaMarcada3=2;
+                        casilla3.classList.add("IAMark");
+                    }else{
+                        console.log("No se puede");
+                        error = true;
+                    }
+                break;
+                    case(4):
+                    if(casillaMarcada4 == 0){
+                        console.log("se puede 4");
+                        casillaMarcada4=2;
+                        casilla4.classList.add("IAMark");
+                    }else{
+                        console.log("No se puede");
+                        error = true;
+                    }
+                break;
+                    case(5):
+                    if(casillaMarcada5 == 0){
+                        console.log("se puede 5");
+                        casillaMarcada5=2;
+                        casilla5.classList.add("IAMark");
+                    }else{
+                        console.log("No se puede");
+                        error = true;
+                    }
+                break;
+                    case(6):
+                    if(casillaMarcada6 == 0){
+                        console.log("se puede 6");
+                        casillaMarcada6=2;
+                        casilla6.classList.add("IAMark");
+                    }else{
+                        console.log("No se puede");
+                        error = true;
+                    }
+                break;
+                    case(7):
+                    if(casillaMarcada7 == 0){
+                        console.log("se puede 7");
+                        casillaMarcada7=2;
+                        casilla7.classList.add("IAMark");
+                    }else{
+                        console.log("No se puede");
+                        error = true;
+                    }
+                break;
+                    case(8):
+                    if(casillaMarcada8 == 0){
+                        console.log("se puede 8");
+                        casillaMarcada8=2;
+                        casilla8.classList.add("IAMark");
+                    }else{
+                        console.log("No se puede");
+                        error = true;
+                    }
+                break;
+                    default:error=true;
+
+            }
+            if(error){
+            console.log("----------Error----------");
+            }else{
+                IACount++;
+            }
+            console.log("Marcando casilla "+newAdd+" total fichas IA-> "+IACount);
+            console.log("casilla2-> "+casillaMarcada2);
+            CheckWin(2);
+        }else{
+            //Comprobar la añadida y la eliminada
+            
+            switch(remove){
+                case(0):
+                    if(casillaMarcada0 == 2){
+                        casillaMarcada0=0;casilla0.classList.remove("IAMark");
+                    }else{
+                        console.log("No se puede eliminar");
+                        error = true;
+                    }
+                break;
+                case(1):
+                    if(casillaMarcada1 == 2){
+                        casillaMarcada1=0;casilla1.classList.remove("IAMark");
+                    }else{
+                        console.log("No se puede eliminar");
+                        error = true;
+                    }
+                break;
+                case(2):
+                    if(casillaMarcada2 == 2){
+                        casillaMarcada2=0;casilla2.classList.remove("IAMark");
+                    }else{
+                        console.log("No se puede eliminar");
+                        error = true;
+                    }
+                break;
+                case(3):
+                    if(casillaMarcada3 == 2){
+                        casillaMarcada3=0;casilla3.classList.remove("IAMark");
+                    }else{
+                        console.log("No se puede eliminar");
+                        error = true;
+                    }
+                break;
+                case(4):
+                    if(casillaMarcada4 == 2){
+                        casillaMarcada4=0;casilla4.classList.remove("IAMark");
+                    }else{
+                        console.log("No se puede eliminar");
+                        error = true;
+                    }
+                break;
+                case(5):
+                    if(casillaMarcada5 == 2){
+                        casillaMarcada5=0;casilla5.classList.remove("IAMark");
+                    }else{
+                        console.log("No se puede eliminar");
+                        error = true;
+                    }
+                break;
+                case(6):
+                    if(casillaMarcada6 == 2){
+                        casillaMarcada6=0;casilla6.classList.remove("IAMark");
+                    }else{
+                        console.log("No se puede eliminar");
+                        error = true;
+                    }
+                break;
+                case(7):
+                    if(casillaMarcada7 == 2){
+                        casillaMarcada7=0;casilla7.classList.remove("IAMark");
+                    }else{
+                        console.log("No se puede eliminar");
+                        error = true;
+                    }
+                break;
+                case(8):
+                    if(casillaMarcada8 == 2){
+                        casillaMarcada8=0;casilla8.classList.remove("IAMark");
+                    }else{
+                        console.log("No se puede eliminar");
+                        error = true;
+                    }
+                break;
+                default:error=true;
+                
+            }
+            //Comprobamos que la casilla que ha marcado la IA no está marcada en caso de que se haya eliminado correctamente
+            if(!error){
+
+            
+                switch(newAdd){
+                    case(0):
+                    if(casillaMarcada0 == 0){
+                        console.log("se puede 0");
+                        casillaMarcada0=2;
+                        casilla0.classList.add("IAMark");
+                    }else{
+                        console.log("No se puede");
+                        error = true;
+                    }
+                    case(1):
+                    if(casillaMarcada1 == 0){
+                        console.log("se puede 1");
+                        casillaMarcada1=2;
+                        casilla1.classList.add("IAMark");
+                    }else{
+                        console.log("No se puede");
+                        error = true;
+                    }
+                    case(2):
+                    if(casillaMarcada2 == 0){
+                        console.log("se puede 2");
+                        casillaMarcada2=2;
+                        casilla2.classList.add("IAMark");
+                    }else{
+                        console.log("No se puede");
+                        error = true;
+                    }
+                    case(3):
+                    if(casillaMarcada3 == 0){
+                        console.log("se puede 3");
+                        casillaMarcada3=2;
+                        casilla3.classList.add("IAMark");
+                    }else{
+                        console.log("No se puede");
+                        error = true;
+                    }
+                    case(4):
+                    if(casillaMarcada4 == 0){
+                        console.log("se puede 4");
+                        casillaMarcada4=2;
+                        casilla4.classList.add("IAMark");
+                    }else{
+                        console.log("No se puede");
+                        error = true;
+                    }
+                    case(5):
+                    if(casillaMarcada5 == 0){
+                        console.log("se puede 5");
+                        casillaMarcada5=2;
+                        casilla5.classList.add("IAMark");
+                    }else{
+                        console.log("No se puede");
+                        error = true;
+                    }
+                    case(6):
+                    if(casillaMarcada6 == 0){
+                        console.log("se puede 6");
+                        casillaMarcada6=2;
+                        casilla6.classList.add("IAMark");
+                    }else{
+                        console.log("No se puede");
+                        error = true;
+                    }
+                    case(7):
+                    if(casillaMarcada7 == 0){
+                        console.log("se puede 7");
+                        casillaMarcada7=2;
+                        casilla7.classList.add("IAMark");
+                    }else{
+                        console.log("No se puede");
+                        error = true;
+                    }
+                    case(8):
+                    if(casillaMarcada8 == 0){
+                        console.log("se puede 8");
+                        casillaMarcada8=2;
+                        casilla8.classList.add("IAMark");
+                    }else{
+                        console.log("No se puede");
+                        error = true;
+                    }
+                    default:error=true;
+
+                }
+            }
+            if(error){
+            console.log("----------Error----------");
+            }
+            CheckWin(2);
+
+
+        }
+    
+    turno = 0;
+}
 
 
 function CheckInput(casillaMarcada, casilla, selected){
@@ -83,6 +404,7 @@ function CheckInput(casillaMarcada, casilla, selected){
             casilla7: casillaMarcada7,
             casilla8: casillaMarcada8,
         }
+        IATime(input);
         console.log(input)
         
         // let resultado = network.run(input);
@@ -96,66 +418,83 @@ function CheckInput(casillaMarcada, casilla, selected){
 
 }
 
-if(turno == 0){
+
     console.log("Game Over-> ",gameOver);
     casilla0.onclick = function(){
         CheckInput(casillaMarcada0,casilla0,0);
-        CheckWin();
+        CheckWin(1);
     }
     casilla1.onclick = function(){
         CheckInput(casillaMarcada1,casilla1,1);
-        CheckWin();
+        CheckWin(1);
     }
     casilla2.onclick = function(){
         CheckInput(casillaMarcada2,casilla2,2);
-        CheckWin();
+        CheckWin(1);
     }
     casilla3.onclick = function(){
         CheckInput(casillaMarcada3,casilla3,3);
-        CheckWin();
+        CheckWin(1);
     }
     casilla4.onclick = function(){
         CheckInput(casillaMarcada4,casilla4,4);
-        CheckWin();
+        CheckWin(1);
     }
     casilla5.onclick = function(){
         CheckInput(casillaMarcada5,casilla5,5);
-        CheckWin();
+        CheckWin(1);
     }
     casilla6.onclick = function(){
         CheckInput(casillaMarcada6,casilla6,6);
-        CheckWin();
+        CheckWin(1);
     }
     casilla7.onclick = function(){
         CheckInput(casillaMarcada7,casilla7,7);
-        CheckWin();
+        CheckWin(1);
     }
     casilla8.onclick = function(){
         CheckInput(casillaMarcada8,casilla8,8);
-        CheckWin();
+        CheckWin(1);
     }
 
     
+if(turno == 1){
+    //! Aqui solo entrará 1 vez
+    console.log("TURNO DE LA IA if general");
+    let input = {
+        casilla0: casillaMarcada0,
+        casilla1: casillaMarcada1,
+        casilla2: casillaMarcada2,
+        casilla3: casillaMarcada3,
+        casilla4: casillaMarcada4,
+        casilla5: casillaMarcada5,
+        casilla6: casillaMarcada6,
+        casilla7: casillaMarcada7,
+        casilla8: casillaMarcada8,
+    }
+    IATime(input);
 }
 
-function CheckWin(){
+
+
+function CheckWin(player){
     if(!gameOver){
 
         //*Horizontal
         
         //Horizontal1
-        if(casillaMarcada0 == 1 && casillaMarcada1 == 1 && casillaMarcada2 == 1){
+        if(casillaMarcada0 == player && casillaMarcada1 == player && casillaMarcada2 == player){
             gameOver = true;
-            alert("Player1 win");
+            // player==1?alert("Victoria!"):console.log("Derrota");
         }
         //Horizontal2
-        if(casillaMarcada3 == 1 && casillaMarcada4 == 1 && casillaMarcada5 == 1){
-            alert("Player1 win");
+        if(casillaMarcada3 == player && casillaMarcada4 == player && casillaMarcada5 == player){
+            
             gameOver = true;
         }
         //Horizontal1
-        if(casillaMarcada6 == 1 && casillaMarcada7 == 1 && casillaMarcada8 == 1){
-            alert("Player1 win");
+        if(casillaMarcada6 == player && casillaMarcada7 == player && casillaMarcada8 == player){
+          
             gameOver = true;
         }
         
@@ -163,33 +502,48 @@ function CheckWin(){
         //*Vertical
         
         //Vertical1
-        if(casillaMarcada0 == 1 && casillaMarcada3 == 1 && casillaMarcada6 == 1){
-            alert("Player1 win");
+        if(casillaMarcada0 == player && casillaMarcada3 == player && casillaMarcada6 == player){
+          
             gameOver = true;
         }
         //Vertical2
-        if(casillaMarcada1 == 1 && casillaMarcada4 == 1 && casillaMarcada7 == 1){
-            alert("Player1 win");
+        if(casillaMarcada1 == player && casillaMarcada4 == player && casillaMarcada7 == player){
+          
             gameOver = true;
         }
         //Vertical1
-        if(casillaMarcada2 == 1 && casillaMarcada5 == 1 && casillaMarcada8 == 1){
-            alert("Player1 win");
+        if(casillaMarcada2 == player && casillaMarcada5 == player && casillaMarcada8 == player){
+            
             gameOver = true;
         }
         
         //*Diagonal
         
         //Diagonal iz to der
-        if(casillaMarcada0 == 1 && casillaMarcada4 == 1 && casillaMarcada8 == 1){
-            alert("Player1 win");
+        if(casillaMarcada0 == player && casillaMarcada4 == player && casillaMarcada8 == player){
+      
             gameOver = true;
         }
         //Diagonal der to iz
-        if(casillaMarcada2 == 1 && casillaMarcada4 == 1 && casillaMarcada6 == 1){
-            alert("Player1 win");
+        if(casillaMarcada2 == player && casillaMarcada4 == player && casillaMarcada6 == player){
+
             gameOver = true;
         }
+        if(gameOver){
+            if(player==1){
+                alert("Victoria!");
+            }else{
+                //Victoria IA
+                alert("Derrota");
+                
+                //Guardar
+                const json = network.toJSON();
+                console.log("Network entrenada:");
+                console.log(json);
+                //net.fromJSON(json);
+            }
+        }
     }
+
     
 }
--- network.js
+++ network.js
@@ -2,20 +2,97 @@
  let network = new brain.NeuralNetwork();
  // let net = new brain.LSTMTimeStep();
 
- //Texto en blanco fondo negro y al reves
+ //TODO Estoy interpretandolo mal, tengo que devolver un output para cada posible posicion de cada casilla,
+ //TODO primero compruebo cual debo eliminar, y luego cual debo añadir
+ // tengo que devolver un mapa completo sobre cual quiero eliminar y cual quiero añadir
+
+
+ //? ACTUALIZACION-> 2 modelos correctos completados
+ //TODO ver que coño hacer para elegir cual tengo que borrar
+ //TODO quizas tener una segunda red neuronal que se encargue de decidir cual borrar
+
  network.train([
-     //Fondo negro, salida blanco
-     {input: {rojo:0,verde:0,azul:0}, output:{color:1}},
-     //Fondo blanco, salida negro
-     {input: {rojo:1,verde:1,azul:1}, output:{color:0}},
+     //Primera jugada
+     {input: {
+        casilla0:0,casilla1:0,casilla2:0,
+        casilla3:0,casilla4:0,casilla5:0,
+        casilla6:0,casilla7:0,casilla8:0},
+        
+        output:[0,0,0,
+                0,0,0,
+                1,0,0]
+    }, 
+
+        //Delete elimina una casilla previamente seleccionada en el caso de que tengamos 3 seleccionadas
+    {input: {
+        casilla0:0,casilla1:0,casilla2:0,
+        casilla3:0,casilla4:0,casilla5:1,
+        casilla6:2,casilla7:0,casilla8:0},
+        
+        output:[1,0,0,
+                0,0,0,
+                0,0,0]
+        },
+
+    // {input: {
+    //     casilla0:2,casilla1:0,casilla2:0,
+    //     casilla3:1,casilla4:0,casilla5:1,
+    //     casilla6:2,casilla7:0,casilla8:0},
+        
+    //     output:{delete:0,add:0.2}},
+
     
-     {input: {rojo:0,verde:1,azul:0}, output:{color:0}}
+    // {input: {
+    //     casilla0:2,casilla1:1,casilla2:2,
+    //     casilla3:1,casilla4:0,casilla5:1,
+    //     casilla6:2,casilla7:0,casilla8:0},
+        
+    //     output:{delete:0,add:0.4}},
+    
+    // //Inputs que implican borrar una casilla
+    // {input: {
+    //     casilla0:2,casilla1:0,casilla2:1,
+    //     casilla3:1,casilla4:2,casilla5:1,
+    //     casilla6:2,casilla7:0,casilla8:0},
+        
+    //     output:{delete:0.6,add:0.8}},
+    // {input: {
+    //     casilla0:2,casilla1:0,casilla2:1,
+    //     casilla3:1,casilla4:0,casilla5:2,
+    //     casilla6:2,casilla7:0,casilla8:1},
+        
+    //     output:{delete:0.6,add:0.4}},
+    // {input: {
+    //     casilla0:1,casilla1:0,casilla2:2,
+    //     casilla3:0,casilla4:0,casilla5:1,
+    //     casilla6:2,casilla7:1,casilla8:2},
+        
+    //     output:{delete:0.8,add:0.4}},
+    // {input: {
+    //     casilla0:2,casilla1:1,casilla2:0,
+    //     casilla3:0,casilla4:2,casilla5:0,
+    //     casilla6:1,casilla7:2,casilla8:1},
+        
+    //     output:{delete:0.0,add:0.3}},
+    // {input: {
+    //     casilla0:1,casilla1:0,casilla2:2,
+    //     casilla3:0,casilla4:1,casilla5:1,
+    //     casilla6:2,casilla7:0,casilla8:2},
+        
+    //     output:{delete:0.2,add:0.7}},
+
+
  ]);
 
- let entrene = document.getElementById("forecast");
- entrene.onclick = function(){
-     let json = network.toJSON(); //Pasar network a json
-     // net.fromJSON(json);
-     // net.forecast(input, 3);
-     console.log(json);
- }
+//  network.train(trainingData,{
+//     log: (error) => console.log("NEURAL ERROR ",error),
+//     logPeriod: 100
+//  });
+
+//  let entrene = document.getElementById("forecast");
+//  entrene.onclick = function(){
+//      let json = network.toJSON(); //Pasar network a json
+//      // net.fromJSON(json);
+//      // net.forecast(input, 3);
+//      console.log(json);
+//  }
